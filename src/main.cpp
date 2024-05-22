6d026b614e772d6515106d5637ab36ece0c41ca5
Tilman Beyer <108127052+TMan91@users.noreply.github.com>
2024-05-22 21:56:53 +0200

    Change main.cpp
    Changed the used Sensor to a photosensitive

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,19 @@
 #include <Arduino.h>
 #include <Adafruit_SSD1306.h>
-#include <vector>
-using namespace std;
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1); // Instanziierung
 
-#define lowerThreshold 2500
-#define upperThreshold 3000
-
 const int analogPin = 34; //Pin für den Analogen Feuchtigkeitssensor
+
+int currentAnalogValue = 0; //Aktueller Wert des Analogen Photosensors
+
 double runtime;           // Zeit seit Boot
-double drinkTime = 0;      //gesamtzeit des Trinkens
-double drinkStart = 0;    //Zeitpunkt bei dem Angefange wurde zu trinken
-double drinkEnd = 0;      //Zeitpunkt bei dem Aufgehört wurde zu trinken
-double currentTime = 0;  //verändernde Variable die hochgeht
+double drinkTime = 0.;      //gesamtzeit des Trinkens
+double drinkStart = 0.;    //Zeitpunkt bei dem Angefange wurde zu trinken
+double currentTime = 0.;  //verändernde Variable die hochgeht
 
-
-int currentAnalogValue = 0;
-int lastAnalogValue = 0;
-//Messwertespeicher
-vector <int> analogValues;
 
 /*Stages:
   1 = Trinkbereit
@@ -53,42 +45,86 @@ void setup() {
   display.clearDisplay();
 }
 
+double getRuntime()
+{
+  return millis() / 1000.0;
+}
 
-void loop() {
+int getanalogPin()
+{
+  return analogRead(analogPin);
+}
 
-  
-  lastAnalogValue = currentAnalogValue;
-  currentAnalogValue = ( currentAnalogValue + analogRead(analogPin) ) / 2 ;
+void stage1()
+{
+  stage = 1;
+  currentTime = 0;
+  drinkTime = 0;
+  Serial.print(">Stage:");
+  Serial.println(stage);
 
-  runtime = millis() / 1000.0;
-  Serial.print(">Analog value:");
+  Serial.print(">Time:");
+  Serial.println(currentTime);
+
+  currentAnalogValue = getanalogPin();
+  Serial.print(">AnalogValue:");
   Serial.println(currentAnalogValue);
+  Serial.print(">DrinkTime:");
+  Serial.println(drinkTime);
 
-  if(stage == 1)
+  display.clearDisplay();
+  display.setTextSize(3);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 20);
+  display.print("Trinken");
+  display.display();
+  delay(2000);
+} 
+
+void stage2()
+{
+
+  while(getanalogPin() > 100)
   {
-    display.clearDisplay();
-    display.setTextSize(3);
-    display.setTextColor(WHITE);
-    display.setCursor(0, 20);
-    display.print("Trinken");
-    display.display();
     stage = 2;
-    delay(2000);
+    Serial.print(">Stage:");
+    Serial.println(stage);
+    Serial.print(">Time:");
+    Serial.println(currentTime);
+
+    currentAnalogValue = getanalogPin();
+    Serial.print(">AnalogValue:");
+    Serial.println(currentAnalogValue);
+    Serial.print(">DrinkTime:");
+    Serial.println(drinkTime);
+
+    //double startTime = millis();
+    //double endTime = millis();
+    //Serial.print(">Time:");
+    //Serial.println(endTime - startTime);
   }
-  else  if(stage == 2 && currentAnalogValue < lowerThreshold)
+
+  drinkStart = millis();
+}
+
+void stage3()
+{
+  currentAnalogValue = getanalogPin();
+  while(currentAnalogValue < 100 || currentTime < 1)
   {
-    drinkStart = runtime;
+    currentAnalogValue = getanalogPin();
     stage = 3;
-  } 
-  else if(stage == 3)
-  {
-    if(currentAnalogValue > upperThreshold)
-    {
-      drinkEnd = runtime;
-      drinkTime = drinkEnd - drinkStart;
-      stage = 4;
-    }
-    currentTime = runtime - drinkStart;
+    Serial.print(">Stage:");
+    Serial.println(stage);
+
+    currentTime = (millis() - drinkStart)/1000.0;
+    currentTime = (double)(int)(currentTime * 100) / 100;
+
+    Serial.print(">Time:");
+    Serial.println(currentTime);
+    Serial.print(">AnalogValue:");
+    Serial.println(currentAnalogValue);
+
     display.clearDisplay();
     display.setTextSize(3);
     display.setTextColor(WHITE);
@@ -96,22 +132,43 @@ void loop() {
     display.print(currentTime);
     display.display();
 
+    //double startTime = millis();
+    //double endTime = millis();
+    //Serial.print(">Time:");
+    //Serial.println(endTime - startTime);
   }
-  else if(stage == 4)
-  {
-    display.clearDisplay();
-    display.setTextSize(3);
-    display.setTextColor(WHITE);
-    display.setCursor(0, 20);
-    display.print(drinkTime);
-    display.display();
-    stage = 1;
-    delay(5000);
-  }
-/*
-  Serial.print(">Analog value:");
-  Serial.println(analogRead(analogPin));
+  
+  drinkTime = (millis() - drinkStart) / 1000.0;
+  drinkTime = (double)(int)(drinkTime * 100) / 100;
+  Serial.print(">DrinkTime:");
+  Serial.println(drinkTime);
+}
 
-*/
+void stage4()
+{
+  stage = 4;
+  Serial.print(">Stage:");
+  Serial.println(stage);
+
+  display.clearDisplay();
+  display.setTextSize(3);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 20);
+  display.print(drinkTime);
+  display.display();
+  delay(5000);
+}
+
+void loop() {
+
+  //runtime = millis() / 1000.0;
+  //Serial.print(">Analog value:");
+  //Serial.println(currentMedian);
+
+  stage1();
+  stage2();
+  stage3();
+  stage4();
+
 }
 
